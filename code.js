6392436967d9b202cb78a5957d982a44143c6e45
masaibar <masaibar.dev@gmail.com>
2020-03-13 17:25:23 +0900

    code.ts側にcsvデータを渡すところまで

--- code.js
+++ code.js
@@ -20,30 +20,38 @@ figma.showUI(__html__);
 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-    }
+    // if (msg.type === 'create-rectangles') {
+    //   const nodes: SceneNode[] = [];
+    //   for (let i = 0; i < msg.count; i++) {
+    //     const rect = figma.createRectangle();
+    //     rect.x = i * 150;
+    //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //     figma.currentPage.appendChild(rect);
+    //     nodes.push(rect);
+    //   }
+    //   figma.currentPage.selection = nodes;
+    //   figma.viewport.scrollAndZoomIntoView(nodes);
+    // }
     if (msg.type === 'rewrite') {
         for (let item of figma.currentPage.children) {
             if (item.type === "TEXT") {
                 yield figma.loadFontAsync({ family: item.fontName["family"], style: item.fontName["style"] });
-                console.log(item.name);
-                console.log(item.characters);
-                console.log(item.fontName);
-                console.log(item.fontName["family"]);
-                console.log(item.fontName["style"]);
+                // console.log(item.name)
+                // console.log(item.characters)
+                // console.log(item.fontName)
+                // console.log(item.fontName["family"])
+                // console.log(item.fontName["style"])
                 item.characters = "fugafuga";
             }
         }
+    }
+    if (msg.type === 'csv') {
+        console.log("csv call");
+        const lines = msg.lines;
+        lines.forEach(line => {
+            console.log(line);
+        });
+        console.log("csv called");
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
--- code.ts
+++ code.ts
@@ -12,36 +12,46 @@ figma.showUI(__html__);
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = async msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  // if (msg.type === 'create-rectangles') {
-  //   const nodes: SceneNode[] = [];
-  //   for (let i = 0; i < msg.count; i++) {
-  //     const rect = figma.createRectangle();
-  //     rect.x = i * 150;
-  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  //     figma.currentPage.appendChild(rect);
-  //     nodes.push(rect);
-  //   }
-  //   figma.currentPage.selection = nodes;
-  //   figma.viewport.scrollAndZoomIntoView(nodes);
-  // }
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    // if (msg.type === 'create-rectangles') {
+    //   const nodes: SceneNode[] = [];
+    //   for (let i = 0; i < msg.count; i++) {
+    //     const rect = figma.createRectangle();
+    //     rect.x = i * 150;
+    //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //     figma.currentPage.appendChild(rect);
+    //     nodes.push(rect);
+    //   }
+    //   figma.currentPage.selection = nodes;
+    //   figma.viewport.scrollAndZoomIntoView(nodes);
+    // }
 
-  if (msg.type === 'rewrite') { 
-    for (let item of figma.currentPage.children) { 
-      if (item.type === "TEXT") {
-        await figma.loadFontAsync({ family: item.fontName["family"], style: item.fontName["style"] })
-        // console.log(item.name)
-        // console.log(item.characters)
-        // console.log(item.fontName)
-        // console.log(item.fontName["family"])
-        // console.log(item.fontName["style"])
-        item.characters = "fugafuga"
-      }
+    if (msg.type === 'rewrite') {
+        for (let item of figma.currentPage.children) {
+            if (item.type === "TEXT") {
+                await figma.loadFontAsync({family: item.fontName["family"], style: item.fontName["style"]})
+                // console.log(item.name)
+                // console.log(item.characters)
+                // console.log(item.fontName)
+                // console.log(item.fontName["family"])
+                // console.log(item.fontName["style"])
+                item.characters = "fugafuga"
+            }
+        }
     }
-  }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+    if (msg.type === 'csv') {
+        console.log("csv call")
+        const lines = msg.lines
+
+        lines.forEach(line => {
+            console.log(line)
+        })
+        console.log("csv called")
+    }
+
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
 };
